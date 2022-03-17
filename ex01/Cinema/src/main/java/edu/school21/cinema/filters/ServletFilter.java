dde2ba5bc98538bfc7af0d605f7e1fc4b58dd703
morrkof <morrkof@gmail.com>
2022-03-18 00:05:00 +0300

    ex01 start work with filter

---
+++ ex01/Cinema/src/main/java/edu/school21/cinema/filters/ServletFilter.java
@@ -0,0 +1,24 @@
+package edu.school21.cinema.filters;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class ServletFilter implements Filter {
+
+    private FilterConfig filterConfig;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
